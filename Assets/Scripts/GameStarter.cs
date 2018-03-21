f491e8644f5d9dd39ab5686423e7a3b648258fb4
Marco Pompa <marcopompa7@gmail.com>
2018-03-21 20:12:38 +0100

    Minor fixes

---
+++ Assets/Scripts/GameStarter.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameStarter : MonoBehaviour {
+
+	bool hasStarted;
+
+	public GameObject startText;
+
+	void Start () {
+		hasStarted = false;
+	}
+	
+	void Update () {
+		if (!hasStarted) {
+			Time.timeScale = 0;
+			startText.gameObject.SetActive(true);
+
+			if (Input.GetKeyDown(KeyCode.Space)) {
+				hasStarted = true;
+				Time.timeScale = 1;
+				startText.gameObject.SetActive(false);
+			}
+		}
+	}
+}
