240bc759553d5d8418ba97563f33e6ac29da03d2
Marco Pompa <marcopompa7@gmail.com>
2018-03-22 15:33:23 +0100

    Slider to adjust music and sfx volume is now working

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class AudioManager : MonoBehaviour {
 
@@ -9,18 +10,25 @@ public class AudioManager : MonoBehaviour {
 	public AudioSource changeColorSound;
 	public AudioSource blockPassedSound;
 	public AudioSource levelCompletedSound;
-	public AudioSource selectionSound;
+	public AudioSource selectionSound, selectionSound2;
 	public AudioSource backToWhiteSound;
 
-	void Start () {
-		
-	}
-	
-	void Update () {
-		
-	}
+	public AudioMixer audioMixer;
 
 	public void PlaySelection () {
 		selectionSound.Play();
 	}
+
+	public void PlayOnHover () {
+		selectionSound2.Play();
+	}
+
+	public void SetMusicVolume (float volume) {
+		audioMixer.SetFloat("musicVolume", volume);
+	}
+
+	public void SetSFXVolume (float volume) {
+		audioMixer.SetFloat("sfxVolume", volume);
+	}
+
 }
